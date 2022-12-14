13e6e7ee0ffe4a96b175c0d2579ab73a5b09e84b
Fabrício Lopes <fabriciolopessilveira@gmail.com>
2022-12-14 13:53:24 -0300

    Atualização das telas de notas e relatórios

--- pages/notas.vue
+++ pages/notas.vue
@@ -6,35 +6,63 @@
       <div class="voltar">
         <a href="/usuario"><img src="/icons/left-arrow.svg" alt="Voltar" /></a>
       </div>
-      <h1 class="texto">Notas!</h1>
+      <p class="texto">Notas de {{ aluno.nome }}</p>
 
-      <div class="texto_lista">
-        <p>Como deseja visualizar:</p>
-        <select class="lista">
-          <option value="">Item1</option>
-          <option value="">Item2</option>
-          <option value="">Item3</option>
-          <option value="">Item4</option>
-        </select>
-      </div>
-      <div class="botao">
-        <button class="processar">Processar</button>
-      </div>
+      <ul class="lista">
+        <li
+          v-for="(disciplina, key) in aluno.disciplinas"
+          :key="key"
+          class="list-item"
+        >
+          <span class="list-title">{{ disciplina.nome }}</span>
+          <Badge
+            v-if="disciplina.frequencia"
+            :variant="presentState(disciplina.frequencia)"
+          >
+            {{ disciplina.frequencia.presenca }}
+            /
+            {{ disciplina.frequencia.total }}
+          </Badge>
+        </li>
+      </ul>
     </main>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import Badge from '~/components/Badge.vue'
 import HeaderComponent from '~/components/Header.vue'
 export default {
   components: {
     HeaderComponent,
+    Badge,
+  },
+
+  computed: {
+    ...mapState('aluno', {
+      aluno: (state) => state,
+    }),
+  },
+
+  methods: {
+    presentState({ presenca, total }) {
+      if (presenca / total < 0.5) {
+        return 'danger'
+      }
+
+      if (presenca / total < 0.75) {
+        return 'warning'
+      }
+
+      return 'success'
+    },
   },
 }
 </script>
 
 <style scoped>
-.header {
+button {
   background-color: #ff66c4;
   width: 100%;
   height: 76px;
@@ -60,41 +88,29 @@ export default {
   margin: 40px 30px;
 }
 
-.texto_lista {
-  margin: 0.5rem;
-}
-
-.texto_lista {
-  display: grid;
-  grid-template-columns: 1fr;
-  margin: 7rem 5rem;
-}
-
 .lista {
-  text-align: center;
-  background: #f2f2f2;
-  border-radius: 15px;
-  height: 3rem;
-  border-style: none;
-}
-
-.botao {
   display: grid;
   grid-template-columns: 1fr;
-  margin: 14rem 9rem;
+  margin-top: 5rem;
+  list-style: none;
+  padding-left: 20px;
+  padding-right: 20px;
 }
 
-.processar {
-  text-align: center;
-  border-radius: 35px;
-  border-style: none;
-  background: rgba(43, 232, 136, 0.3);
-  color: #349061;
-  font-weight: 700;
-  height: 2rem;
+.list-item {
+  background-color: #f2f2f2;
+  margin: 1rem 2rem;
+  border-radius: 4px;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1.25rem;
 }
 
-.texto_lista p {
-  margin-left: 0.5rem;
+.list-item span.list-title {
+  font-size: 1.5rem;
+  font-weight: 400;
+  color: #333;
 }
 </style>
--- pages/relatorios.vue
+++ pages/relatorios.vue
@@ -6,7 +6,20 @@
       <div class="voltar">
         <a href="/usuario"><img src="/icons/left-arrow.svg" alt="Voltar" /></a>
       </div>
-      <p class="texto">Relatórios!</p>
+      <h1 class="texto">Relatórios!</h1>
+
+      <div class="texto_lista">
+        <p>Como deseja visualizar:</p>
+        <select class="lista">
+          <option value="">Item1</option>
+          <option value="">Item2</option>
+          <option value="">Item3</option>
+          <option value="">Item4</option>
+        </select>
+      </div>
+      <div class="botao">
+        <button class="processar">Processar</button>
+      </div>
     </main>
   </div>
 </template>
@@ -21,13 +34,7 @@ export default {
 </script>
 
 <style scoped>
-body {
-  margin: 0px;
-  padding: 0px;
-  border: 0px;
-}
-
-button {
+.header {
   background-color: #ff66c4;
   width: 100%;
   height: 76px;
@@ -51,5 +58,43 @@ button {
   font-size: 2rem;
   font-weight: 400;
   margin: 40px 30px;
+}
+
+.texto_lista {
+  margin: 0.5rem;
+}
+
+.texto_lista {
+  display: grid;
+  grid-template-columns: 1fr;
+  margin: 7rem 5rem;
+}
+
+.lista {
+  text-align: center;
+  background: #f2f2f2;
+  border-radius: 15px;
+  height: 3rem;
+  border-style: none;
+}
+
+.botao {
+  display: grid;
+  grid-template-columns: 1fr;
+  margin: 14rem 9rem;
+}
+
+.processar {
+  text-align: center;
+  border-radius: 35px;
+  border-style: none;
+  background: rgba(43, 232, 136, 0.3);
+  color: #349061;
+  font-weight: 700;
+  height: 2rem;
+}
+
+.texto_lista p {
+  margin-left: 0.5rem;
 }
 </style>
